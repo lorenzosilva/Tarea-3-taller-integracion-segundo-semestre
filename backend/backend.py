36637069d5557fe0ef1fca93e2fe641171b3ee4b
lorenzosilva <lssilva1@uc.cl>
2024-11-07 21:17:57 -0300

    'final3'

--- backend/backend.py
+++ backend/backend.py
@@ -29,7 +29,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://672d5618ed6edb8f8a9884c6--pregunta-peliculas.netlify.app"],  # Adjust if deploying to a different domain
+    allow_origins=["https://672d5618ed6edb8f8a9884c6--pregunta-peliculas.netlify.app", "https://pregunta-peliculas.netlify.app"],  # Adjust if deploying to a different domain
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
